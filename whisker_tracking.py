c8f8cf28a30f33165ac36af49cd9473498b0697b
gitelian <gitelian@berkeley.edu>
2015-09-09 16:17:02 -0700

    Neural decoding

--- whisker_tracking.py
+++ whisker_tracking.py
@@ -1,6 +1,8 @@
 #!/bin/bash
 from neuroanalyzer import *
+from corrstats import *
 import seaborn as sns
+import statsmodels.stats.multitest as smm
 
 def get_wt_run_vals(wtmat, trials_ran_dict, cond_ind_dict, vel_mat, trial_time):
     num_trials_ran = [sum(trials_ran_dict[x]) for x in np.sort(trials_ran_dict.keys())]
@@ -10,25 +12,38 @@ def get_wt_run_vals(wtmat, trials_ran_dict, cond_ind_dict, vel_mat, trial_time):
     amp_list = list()
     num_samples = vel_mat.shape[1]
     vel_list = list()
+    ang_list = list()
     for cond_ind, cond in enumerate(np.sort(trials_ran_dict.keys())):
         print(cond)
         temp_setpoint = np.zeros((num_trials_ran[cond_ind], num_frames))
         temp_amp = np.zeros((num_trials_ran[cond_ind], num_frames))
+        temp_ang = np.zeros((num_trials_ran[cond_ind], num_frames))
         temp_vel = np.zeros((num_trials_ran[cond_ind], num_samples))
         temp_inds = np.where(np.array(trials_ran_dict[cond]) == True)[0]
         run_inds  = cond_ind_dict[cond][temp_inds]
         print(run_inds)
 
         # Given a condition iterate through all of its running trials
+        good_inds = list()
         for trial_ind,runind in enumerate(run_inds):
+
+            temp_sp = wtmat['setPointMat'][runind]
+            temp_a  = wtmat['angleMat'][runind]
+            bad_set_point = np.sum(np.logical_or(temp_sp < 0, temp_sp > 200)) > 0
+            bad_angle = np.sum(np.logical_or(temp_a < 0, temp_a > 200)) > 0
+            if not bad_set_point and not bad_angle:
+                good_inds.append(trial_ind)
+
             temp_setpoint[trial_ind, :] = wtmat['setPointMat'][runind]
             temp_amp[trial_ind, :] = wtmat['ampMat'][runind]
+            temp_ang[trial_ind, :] = wtmat['angleMat'][runind]
             temp_vel[trial_ind, :] = vel_mat[runind, :]
-        set_point_list.append(temp_setpoint)
-        amp_list.append(temp_amp)
-        vel_list.append(temp_vel)
+        set_point_list.append(temp_setpoint[good_inds, :])
+        amp_list.append(temp_amp[good_inds, :])
+        vel_list.append(temp_vel[good_inds, :])
+        ang_list.append(temp_ang[good_inds, :])
 
-    return camTime, set_point_list, amp_list, vel_list
+    return camTime, set_point_list, amp_list, vel_list, ang_list
 
 def velocity_analysis_parameters(wtmat, trials_ran_dict, cond_ind_dict, vel_mat, trial_time):
     numframes2bin = 50
@@ -57,9 +72,10 @@ def velocity_analysis_parameters(wtmat, trials_ran_dict, cond_ind_dict, vel_mat,
                 np.nanmean(wtmat['ampMat'][runind].reshape(-1, numframes2bin), axis=1)))
             temp_vel = np.concatenate((temp_vel,
                 np.nanmean(vel_mat[runind, run_time_ind].reshape(-1, numframes2bin), axis=1)))
-        set_point_list.append(temp_setpoint)
-        amp_list.append(temp_amp)
-        vel_list.append(temp_vel)
+        good_inds = np.where(np.logical_and(temp_setpoint >= 0, temp_setpoint <= 200))
+        set_point_list.append(temp_setpoint[good_inds])
+        amp_list.append(temp_amp[good_inds])
+        vel_list.append(temp_vel[good_inds]) # removes columns
 
     return camTime, set_point_list, amp_list, vel_list
 
@@ -350,9 +366,133 @@ if __name__ == "__main__":
 ##### Make set-point vs runspeed plots #####
 ##### Make set-point vs runspeed plots #####
 
-    [camTime, set_point_vals, amp_vals, vel_vals] = velocity_analysis_parameters(wtmat,
-            trials_ran_dict, cond_ind_dict, vel_mat, trial_time)
+    fids = ['1034', '1038', '1044', '1051', '1054', '1058', '1062', '1067']
+    region = 'vS1'
 
+    sns.set_context("poster")
+    scorr_coefs = np.empty((0, 2))
+    test_pvals  = np.empty((0, 1))
+
+    sp_diff   = np.empty((0, 1))
+    sp_ttest_pvals = np.empty((0, 1))
+    amp_diff   = np.empty((0, 1))
+    amp_ttest_pvals = np.empty((0, 1))
+
+    ang_dict = dict()
+
+    for fid in fids:
+        usr_dir = os.path.expanduser('~')
+        sorted_spikes_dir_path = usr_dir + '/Documents/AdesnikLab/SortedSpikes/'
+        fid_region = 'fid' + fid + '_' + region
+        sort_file_paths = glob.glob(sorted_spikes_dir_path + fid_region + '*.mat')
+
+        data_dir_path = usr_dir + '/Documents/AdesnikLab/Data/'
+        data_dir_paths  = glob.glob(data_dir_path + fid + '*.dat')
+
+        # Calculate runspeed
+        run_mat = load_run_file(data_dir_paths[0]).value
+        vel_mat, trial_time = calculate_runspeed(run_mat)
+
+        # Get stimulus id list
+        stim = load_stimsequence(data_dir_paths[0])
+
+        # Create running trial dictionary
+        # Strict running thresholds
+        cond_ind_dict,trials_ran_dict = classify_run_trials(stim, vel_mat,
+                trial_time, stim_start=1.25, stim_stop=2.50, mean_thresh=400,
+                sigma_thresh=150, low_thresh=200, display=False)
+
+        vel_mat = vel_mat*(2*np.pi*6)/360.0
+
+        hsv_mat_path = glob.glob(usr_dir + '/Documents/AdesnikLab/Processed_HSV/FID' + fid + '-data*.mat')[0]
+        wtmat = h5py.File(hsv_mat_path)
+
+
+        [camTime, set_point_vals, amp_vals, vel_vals] = velocity_analysis_parameters(wtmat,
+                trials_ran_dict, cond_ind_dict, vel_mat, trial_time)
+
+        camTime, set_point_list, amp_list, vel_list, ang_list = get_wt_run_vals(wtmat, trials_ran_dict,
+                cond_ind_dict, vel_mat, trial_time)
+
+        ang_dict[fid] = ang_list
+
+        sp_samples_list  = [np.nanmean(x[:, 750:1000], axis=1) for x in set_point_list]
+        sp_samples_list  = [np.delete(x, np.where(np.isnan(x))) for x in sp_samples_list] # remove nans
+
+        amp_samples_list  = [np.nanmean(x[:, 750:1000], axis=1) for x in amp_list]
+        amp_samples_list  = [np.delete(x, np.where(np.isnan(x))) for x in amp_samples_list] # remove nans
+
+        for i in range(9):
+
+            ##### Compute Spearmans correlation coefficient on set-point vs
+            ##### runspeed. Calculated if pre vs post trimming coefficients are
+            ##### statistically different using Fisher's z-transform
+            pre_slow_inds = np.where(vel_vals[i] < 40)
+            pre_vel = np.delete(vel_vals[i], pre_slow_inds)
+            pre_sp  = np.delete(set_point_vals[i], pre_slow_inds)
+
+            post_slow_inds = np.where(vel_vals[i+9] < 40)
+            post_vel = np.delete(vel_vals[i+9], post_slow_inds)
+            post_sp  = np.delete(set_point_vals[i+9], post_slow_inds)
+
+            pre_scorr  = sp.stats.spearmanr(pre_vel, pre_sp)
+            post_scorr = sp.stats.spearmanr(post_vel, post_sp)
+
+            scorr_coefs = np.concatenate((scorr_coefs,
+                np.asarray((pre_scorr[0], post_scorr[0])).reshape(1,2)), axis=0)
+            test_pvals  = np.concatenate((test_pvals,
+                independent_corr(pre_scorr[0], post_scorr[0], pre_sp.shape[0], post_sp.shape[0])[1].reshape(1,1)), axis=0)
+
+            ##### Compute ttest on mean set-point measurements and difference
+            ##### in means for the stimulus analysis window.
+            sp_diff   = np.concatenate((sp_diff,
+                np.diff((np.nanmean(sp_samples_list[i]), np.nanmean(sp_samples_list[i+9]))).reshape(1,1)), axis=0)
+            sp_ttest_pvals = np.concatenate((sp_ttest_pvals,
+                sp.stats.ttest_ind(sp_samples_list[i], sp_samples_list[i+9])[1].reshape(1,1)), axis=0)
+
+            ##### Compute ttest on mean amplitude measurements and difference
+            ##### in means for the stimulus analysis window.
+            amp_diff   = np.concatenate((amp_diff,
+                np.diff((np.nanmean(amp_samples_list[i]), np.nanmean(amp_samples_list[i+9]))).reshape(1,1)), axis=0)
+            amp_ttest_pvals = np.concatenate((amp_ttest_pvals,
+                sp.stats.ttest_ind(amp_samples_list[i], amp_samples_list[i+9])[1].reshape(1,1)), axis=0)
+
+    rej, pval_corr = smm.multipletests(test_pvals.reshape(test_pvals.shape[0],), alpha=0.05, method='simes-hochberg')[:2]
+    print(float(sum(rej))/len(rej))
+    corr_diff = np.diff(scorr_coefs, axis=1)
+    a = np.concatenate((np.ones((72,1)), np.ones((72,1))*2),axis=1)
+
+    fig = plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.plot(a.T, scorr_coefs.T, '-o')
+    plt.xlim(0.5, 2.5)
+    plt.xticks([1, 2], ['Pre-trim', 'Post-trim'])
+    plt.title('Spearman correlation coefficiant')
+    plt.subplot(1, 2, 2)
+    plt.hist(corr_diff, np.arange(-0.5, 0.5, 0.05))
+    plt.xlim(-0.5, 0.5)
+    plt.title('Change in Spearman correlation coefficiant')
+
+    sp_rej, sp_pval_corr = smm.multipletests(sp_ttest_pvals.reshape(test_pvals.shape[0],), alpha=0.05, method='simes-hochberg')[:2]
+    print('set-point fraction significant: ' + str(float(sum(sp_rej))/len(sp_rej)))
+
+    amp_rej, amp_pval_corr = smm.multipletests(amp_ttest_pvals.reshape(test_pvals.shape[0],), alpha=0.05, method='simes-hochberg')[:2]
+    print('amplitude fraction significant: ' + str(float(sum(amp_rej))/len(amp_rej)))
+
+    fig = plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.hist(sp_diff, np.arange(-17, 17), align='left')
+    plt.xlim(-15, 15)
+    plt.xlabel('set-point difference (deg)')
+
+
+    plt.subplot(1, 2, 2)
+    plt.hist(amp_diff, np.arange(-17, 17, 0.5), align='left')
+    plt.xlim(-5, 5)
+    plt.xlabel('amplitude difference (deg)')
+
+##### set-point vs runspeed #####
+##### set-point vs runspeed #####
     fig = plt.figure(figsize=(24, 14))
     for i in range(1,10):
         plt.subplot(3, 4, i)
@@ -360,16 +500,10 @@ if __name__ == "__main__":
         plt.scatter(vel_vals[i-1+9], set_point_vals[i-1+9], c='r', alpha=0.7)
         plt.title('Pos ' + str(i))
         plt.xlim(0, 120)
-        plt.ylim(60, 160)
+#        plt.ylim(60, 160)
     plt.show()
 
-
-
-
-
-
-
-    camTime, set_point_list, amp_list, vel_list = get_wt_run_vals(wtmat, trials_ran_dict,
+    camTime, set_point_list, amp_list, vel_list, ang_list = get_wt_run_vals(wtmat, trials_ran_dict,
             cond_ind_dict, vel_mat, trial_time)
 
     set_point_cum, amp_cum, angle_cum, run_cum, total_trials = \
@@ -534,7 +668,9 @@ if __name__ == "__main__":
         n2 = len(sp_samples_list[i+9])
         var1 = np.nanvar(sp_samples_list[i])
         var2 = np.nanvar(sp_samples_list[i+9])
-        sp_error_diff.append(np.sqrt(var1/n1 + var2/n2)*sp.stats.t.ppf((1+0.95)/2.0, n1+n2-2))
+        var_pool = ( (n1 - 1)*var1 + (n2 - 1)*var2)/(n1 + n2 -2)
+        var_err  = np.sqrt(var_pool/n1 + var_pool/n2)
+        sp_error_diff.append(var_err*sp.stats.t.ppf((1+0.95)/2.0, n1+n2-2))
     x = np.arange(1, 10)
     # plot tuning curve
     b1 = plt.errorbar(x, sp_mean_diff[:9], yerr=sp_error_diff[:9], fmt='-o', color='k')
@@ -554,9 +690,11 @@ if __name__ == "__main__":
     for i in range(9):
         n1 = len(vl_samples_list[i])
         n2 = len(vl_samples_list[i+9])
-        var1 = np.nanvar(vl_samples_list[i])
-        var2 = np.nanvar(vl_samples_list[i+9])
-        vl_error_diff.append(np.sqrt(var1/n1 + var2/n2)*sp.stats.t.ppf((1+0.95)/2.0, n1+n2-2))
+        var1 = np.nanvar(sp_samples_list[i])
+        var2 = np.nanvar(sp_samples_list[i+9])
+        var_pool = ( (n1 - 1)*var1 + (n2 - 1)*var2)/(n1 + n2 -2)
+        var_err  = np.sqrt(var_pool/n1 + var_pool/n2)
+        vl_error_diff.append(var_err*sp.stats.t.ppf((1+0.95)/2.0, n1+n2-2))
     x = np.arange(1, 10)
     # plot tuning curve
     b1 = plt.errorbar(x, vl_mean_diff[:9], yerr=vl_error_diff[:9], fmt='-o', color='k')
@@ -634,6 +772,75 @@ if __name__ == "__main__":
 
     plt.suptitle(fid + ' cumulative set-point and runspeed distributions')
     plt.show()
+
+##### test PSD confidence interval stuff  #####
+    params.Fs = 500;
+    params.tapers = [3 5];
+    params.fpass = [1:250];
+    params.err = [1 0.95];
+    params.trialave = 1;
+
+    test = ang_dict['1062']
+    pos = test[0].T
+
+
+
+
+
+##### test PSD confidence interval stuff  #####
+def angle_psd(ang_list, camTime, start=1.5, stop=2.5):
+    analysis_inds = np.where(np.logical_and(camTime >= start, camTime <= stop))
+    pxx_list = list()
+    for angle_traces in ang_list:
+        Pxx_all = np.zeros((257, 0))
+        for i, trace in enumerate(angle_traces):
+            if np.sum(np.where(np.isnan(trace))[0]) == 0:
+                padded_trace = np.pad(trace, ((100,100)), mode='constant', constant_values=0)
+                Fxx, Pxx = sp.signal.welch(padded_trace, fs=500, window='hanning', nperseg=512,
+                        noverlap=256, detrend='constant')
+                Pxx_all = np.concatenate((Pxx_all, Pxx.reshape(257,1)), axis=1)
+        pxx_list.append(Pxx_all)
+
+    pxx = list()
+    for trial, pxx_all in enumerate(pxx_list):
+        print('on trial ' + str(trial))
+        pxxc_upper = list()
+        pxxc_lower = list()
+        boot_samples = np.zeros((10000, 257))
+        n = pxx_all.shape[1]
+        for sample in range(10000):
+            for i in range(257):
+                boot_samples[sample, i] = np.mean(np.random.choice(pxx_all[i,:], n))
+
+        # Calculate 95% Confidence Interval from bootstrap distribution
+        for i in range(257):
+            temp = np.percentile(boot_samples[:, i], [0.025, 0.975])
+            pxxc_upper.append(temp[1])
+            pxxc_lower.append(temp[0])
+        pxx.append( (pxx_all.mean(axis=1), pxxc_lower, pxxc_upper))
+
+
+#    Pxx_all_mean = Pxx_all.mean(axis=1)
+#    P = 257 # number of estimates in the Welch function also degree of freedom
+#    alpha = 1 - 0.95
+#    v = 2*P
+#    c = sp.stats.chi2.ppf([1 - alpha / 2.0, alpha / 2.0], v)
+#    c = v / c
+#    Pxxc_lower = Pxx_all_mean*c[0]
+#    Pxxc_upper = Pxx_all_mean*c[1]
+    plt.figure()
+    for t in range(9):
+        plt.subplot(3,4,t+1)
+        plt.plot(Fxx, pxx[t][0], 'k')
+        plt.fill_between(Fxx, pxx[t][0]- pxx[t][1], pxx[t][0]+ pxx[t][2],
+                facecolor='k', alpha=0.3)
+        plt.plot(Fxx, pxx[t+9][0], 'r')
+        plt.fill_between(Fxx, pxx[t+9][0]- pxx[t+9][1], pxx[t+9][0]+ pxx[t+9][2],
+                facecolor='r', alpha=0.3)
+        plt.xlim(0,35)
+        plt.yscale('log')
+    plt.show()
+
 
 ##### set-point spike triggered averages  #####
 #    pos = 9
